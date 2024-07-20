2d40935de7676520184f4d5ece7005baf5582357
Kumkummm-20 <rohitwenty06@gmail.com>
2024-07-20 11:29:50 +0530

    added day2.js file

---
+++ day2.js
@@ -0,0 +1,118 @@
+// activity 1 : 
+//task 1 
+let num1 = 10, num2=15;
+let sum = num1 + num2;
+console.log(sum);  // Output : 25
+
+//task 2 
+let num3 = 10, num4=15;
+let sub = num3 - num4;
+console.log(sub);  // Output : -5
+
+//task 3 
+let num5 = 10, num6=15;
+let multiply = num5 * num6;
+console.log(multiply);  // Output : 150
+
+//task 4 
+let num7 = 15, num8=10;
+let divide = num7 / num8;
+console.log(divide);  // Output : 1.5
+
+//task 5 
+let num9 = 15, num10=10;
+let remainder= num9 % num10;
+console.log(remainder);  // Output : 5
+
+
+// activity 2:
+
+//task6 
+let num11 = 5 ;
+num11 += 10;
+console.log(num11);   // Output : 15
+
+//task7
+let num12 =15 ;
+num12 -= 10;
+console.log(num12);   // Output : 5
+
+
+//activity3 :
+
+//task8
+let num13 = 5 , num14= 10;
+console.log(num13>num14);    // Output : false
+console.log(num13<num14);    // Output : true
+
+//task9
+let num15 = 5 , num16= 5;
+console.log(num15>=num16);    // Output : true
+console.log(num15<=num16);    // Output : true
+
+//task10
+let num17 = 5 , num18= "5" ;
+console.log(num17==num18);    // Output : true
+console.log(num17===num18);    // Output : false
+
+
+//activity 4 : 
+
+//task11
+const num19 = 10, num20 = 3;
+console.log((num19 < 6) && (num20< 5));
+// Output:false
+
+//task12
+const num21 = 10, num22 = 3;
+console.log((num21 < 6) && (num22< 5));
+// Output: false
+
+//task13
+let num23 = 5  ;
+console.log(!(num23==5));    // Output : false
+
+
+// activity 5 :
+
+//task14 
+let num24 = -3;
+let result = num24>0 ? "positive" : "negative";
+console.log(result ) ;  // Output : negative
+
+
+// Feature request :
+
+// #1 
+const number1= 20 ; number2 = 10;
+var add1 = number1 + number2;
+var sub1 = number1 - number2;
+var multiply1 = number1 * number2;
+var division1 = number1 / number2;
+var remainder1 = number1 % number2;
+
+console.log(add1);         //Output: 30
+console.log(sub1);         // Output: 10
+console.log(multiply1);    // Output: 200
+console.log(division1 );   // Output: 2
+console.log(remainder1);   // Output: 0
+
+
+// #2
+let number3=20 , number4="10" ;
+let result2 = (number3 > number4) && (number3 < number4) ;
+let result3 = (number3 > number4) || (number3 > number4) ;
+let result4 = !(number3=== number4) ;
+
+console.log(result2 ) ;  // Output : false
+console.log(result3 ) ;  // Output : true
+console.log(result4 ) ;  // Output : true
+
+
+// #3 
+let number5 = 10;
+let result5 = number5>0 ? "positive" : "negative";
+console.log(result5 ) ;  // Output : positive
+
+
+//  DAY 2 DONE
